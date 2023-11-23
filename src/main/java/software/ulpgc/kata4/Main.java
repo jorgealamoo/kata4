3735a3ff8d3497078e2ee7e905695ff0b4dbb0a1
Jorge Alamo <jorgealamosan@gmail.com>
2023-11-23 21:01:25 +0000

    Class Main

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -1,4 +1,22 @@
 package software.ulpgc.kata4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(urlOf("chinook.db"))) {
+            SqliteTrackLoader trackLoader = new SqliteTrackLoader(connection);
+            List<Track> tracks = trackLoader.loadAll();
+            for (Track track : tracks) {
+                System.out.println(track);
+            }
+        }
+    }
+
+    private static String urlOf(String filename) {
+        return "jdbc:sqlite:" + filename;
+    }
 }
